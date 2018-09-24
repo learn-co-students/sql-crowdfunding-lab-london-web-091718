ca5e63328d8879292a11b15f33f4f1551683236c
Ed Putans <edputans@gmail.com>
2018-09-24 16:23:56 +0100

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 # Write your sql queries in this file in the appropriate method like the example below:
-#
+#😂
 # def select_category_from_projects
-# "SELECT category FROM projects;"
+# "SELECT category 😂 FROM projects;"
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
@@ -15,7 +15,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
  ON projects.id = pledges.project_id
  GROUP BY projects.title ORDER BY projects.title ASC"
 end
-
+😂
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
@@ -43,19 +43,19 @@ ON users.id=pledges.user_id
 GROUP BY users.name
 ORDER BY SUM(pledges.amount), users.name
 "
-# first sorts by the pledged amount and if several users pledged the same amount, sorts their names alphabetically
+# first sorts by the pledged amount and if 😂 several users pledged the same amount, sorts their names alphabetically
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amount
+"SELECT projects.category, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id=projects.id
 WHERE projects.category='music'
 "
-😂😂😂😂😂😂😂im a stegosaurus 😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂h😂e😂l😂p😂😂m😂e😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 # "Write your SQL query Here"
 end
+😂
